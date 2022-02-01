de2c8e1b2ac93202ccb8fb278306ef8af960f2e7
Rineesh CH <42416458+rineeshch@users.noreply.github.com>
2022-02-02 03:40:39 +0530

    initial commit

---
+++ malayalam_readfromfile_tts.py
@@ -0,0 +1,8 @@
+import io
+from gtts import gTTS
+from playsound import playsound
+fp= io.open("book.txt", mode="r", encoding="utf-8")
+book_content=fp.read()
+ob=gTTS(book_content,lang='ml')
+ob.save("book.mp3")
+playsound("book.mp3")
